353ffedadd0375969f7b06d2e14d27966799bb2d
Baguibassa <57837103+Baguibassa@users.noreply.github.com>
2019-11-16 18:31:36 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,37 +6,30 @@
 
 # This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  s <- NULL # initialization of s
-  # reset value with a new matrix
+  s <- NULL
   set <- function(y) {
-    x <<- y # assign the value of y to the x object in the parent environment
-    s <<- NULL # assign the value of NULL to the s object in the parent environment
+    x <<- y 
+    s <<- NULL
   }
-  # get the value of matrix
-  get <- function() {
-    x
-  } 
-  setsolve <- function(solve) s <<- solve # set the value of the solve
-  getsolve <- function() s # get the value of the solve
-  # gives the name 'set', 'get', 'setsolve' and 'getsolve' respectively to the 
-  #set(), get(), setsolve() and getsolve() function defined above
+  get <- function() x
+  setsolve <- function(solve) s <<- solve 
+  getsolve <- function() s
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
 # This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-# If the inverse has already been calculated (and the matrix has not changed), then the 
-#cachesolve should retrieve the inverse from the cache.
+# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  s <- x$getsolve() # attempts to retrieve a solve from the object passed in as the argument
-  if (!is.null(s)) { # checks to see whether s is NULL
+  s <- x$getsolve()
+  if (!is.null(s)) { 
     message("getting cached data")
-    return(s) # if the s value is not equal to NULL, return a valid cached solve value to the parent environment
+    return(s)
   }
   # If the result of !is.null(s) is FALSE
-  data <- x$get() # cacheSolve() gets the matrix from the input object
-  s <- solve(data, ...) # calculates a solve
-  x$setsolve(s) # uses the setsolve() function on the input object to set the solve in the input object
-  s # returns the value of the solve to the parent environment by printing the solve object
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
